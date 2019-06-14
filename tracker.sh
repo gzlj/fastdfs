8099454b50e3077feb5578422361eca615863a04
gzlj <34568013+gzlj@users.noreply.github.com>
2019-06-14 21:36:58 +0800

    Update tracker.sh
    cofig file will not be changed after a restart

--- tracker.sh
+++ tracker.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+if [ ! -f /initialized ]; then {
+touch /initialized
 sed -i "s#\(port\).*#\1=$TRACKER_PORT#" /etc/fdfs/tracker.conf
 sed -i "s#\(base_path\).*#\1=$TRACKER_BASE_PATH#" /etc/fdfs/tracker.conf
-
+}
+fi
 /usr/bin/fdfs_trackerd /etc/fdfs/tracker.conf restart
 tail -f /export/fastdfs/tracker/logs/trackerd.log
